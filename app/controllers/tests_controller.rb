66a7e1a4ee7cccc3d6f03478d0382e018c334ab8
swati-bacancy <swati.chauhan@bacancytechnology.com>
2018-08-28 12:51:17 +0530

    CRUD of test

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,48 @@
+class TestsController < ApplicationController
+  before_action :find_test, only:[:show, :edit, :update, :destroy]
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to tests_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to tests_path
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+  private
+
+  def test_params
+    params.require(:test).permit(:name)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,4 @@
 class Test < ApplicationRecord
-  has_and_belongs_to_many :questions  
+  belongs_to :student
+  has_and_belongs_to_many :questions
 end
