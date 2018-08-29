1fc79a286824e594512774d9f68f2370bcc476b3
poonam-Bacancy <poonam.shah@bacancytechnology.com>
2018-08-29 16:28:50 +0530

    create CRUD for question and Option

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-  Rails.application.routes.draw do
+Rails.application.routes.draw do
 
-      resources :tests
-      resources :students
+ resources :tests
+ resources :students
+ resources :questions
+
+ root 'questions#index'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  end
+end
--- db/migrate/20180827135928_create_options.rb
+++ db/migrate/20180827135928_create_options.rb
@@ -3,6 +3,7 @@ class CreateOptions < ActiveRecord::Migration[5.1]
     create_table :options do |t|
       t.string :option
       t.text :option_value
+      t.boolean :is_answer
       t.references :question, foreign_key: true
 
       t.timestamps
