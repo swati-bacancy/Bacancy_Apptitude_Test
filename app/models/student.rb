056cae483802c1827b33b62db6382bf86b08cef1
poonam-Bacancy <poonam.shah@bacancytechnology.com>
2018-09-03 18:26:04 +0530

    Apply validations and integrate theme

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,9 @@ class Student < ApplicationRecord
   belongs_to :test
   has_many :student_answers
   has_one :result
+
+  validates :name,:course,:roll_number,:collage_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :mobile_number, presence: true, numericality: { only_integer: true }
+
 end
