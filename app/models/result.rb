fd251a6a4a3efb365765cd5956316a74395ec2df
NensiMakwana <nensi.makwana@bacancytechnology.com>
2018-09-10 13:03:30 +0530

    Enrollment number in export csv

--- app/models/result.rb
+++ app/models/result.rb
@@ -3,12 +3,13 @@ class Result < ApplicationRecord
   belongs_to :test
 
   def self.to_csv(options = {})
-    desired_columns = %w(id student_name student_email course collage_name total_questions attempted_questions correct_answer)
+    desired_columns = %w(student_name student_enroll_number student_email course collage_name total_questions attempted_questions correct_answer)
     CSV.generate(options) do |csv|
       csv << desired_columns
       all.each do |result|
         attributes = result.attributes
         attributes["student_name"] = result.student.name
+        attributes["student_enroll_number"] = result.student.roll_number
         attributes["student_email"] = result.student.email
         attributes["course"] = result.student.course
         attributes["collage_name"] = result.student.collage_name
